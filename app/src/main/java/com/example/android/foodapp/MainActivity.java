39beb9fcd05b63f170e5a167840bbdb8e111dec8
tanveertkd <hazarikatanveer12@gmail.com>
2018-06-07 02:53:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/foodapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.foodapp;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
